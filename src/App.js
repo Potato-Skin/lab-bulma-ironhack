0b6423e37c859a62c84b9dac4715bb32c691c894
Tomsylva <tomsylvanz@gmail.com>
2021-05-01 16:53:12 +0200

    iteration 2 complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./navbar/Navbar.js";
+import FormField from "./formfield/FormField";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
     </div>
   );
 }
