fbade466faa97601fbe28f153e3cea939ffd70d5
Tomsylva <tomsylvanz@gmail.com>
2021-05-01 16:37:52 +0200

    navbar element added

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav class="navbar is-transparent">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https:/bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: a modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          ></img>
+        </a>
+        <a class="navbar-item" href="#">
+          Home
+        </a>
+        <div class="navbar-item navbar-end" id="buttons">
+          <button class="button is-primary">Login</button>
+          <button class="button is-info">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
