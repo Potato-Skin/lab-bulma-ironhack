90f193d1a4ab556d86c0bee34780f7bea76f3c5d
luisjhc <ljoaquincuenca@gmail.com>
2021-05-03 17:22:08 +0200

    done without styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import FormField from "./formfield/FormField";
 import CoolButton from "./coolButton/CoolButton";
+import Signup from "./signup/Signup";
+import Container from "./container/Container";
+import Message from "./message/Message";
 
 function App() {
   return (
@@ -12,6 +15,14 @@ function App() {
       <FormField label="Email" type="email" placeholder="Email" />
       <CoolButton className="is-rounded is-danger">Button 1</CoolButton>
       <CoolButton isSuccess>Button 2</CoolButton>
+      <Signup />
+      <Container>
+        <Message isInfo title="Hello World">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at
+          sapien et eros lacinia luctus.
+          <strong> Pellentesque risus mi</strong>.
+        </Message>
+      </Container>
     </div>
   );
 }
